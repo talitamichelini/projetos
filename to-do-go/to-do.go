bf0ea575d4069622bf939fa50629e0c2670f4e49
Talita Michelini <talita.michelini@yahoo.com.br>
2026-07-06 16:28:16 -0300

    Finalizado Projeto To-DO-API

--- to-do-go/to-do.go
+++ to-do-go/to-do.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -26,12 +27,16 @@ var tasks = []Task{
 		Done:  true,
 	},
 }
+var nextID = 3
 
 func main() {
 
 	r := chi.NewRouter()
 
 	r.Get("/tasks", getTasks)
+	r.Post("/tasks", createTask)
+	r.Put("/tasks/{id}", updateTask)
+	r.Delete("/tasks/{id}", deleteTask)
 
 	fmt.Println("Servidor iniciado na porta 8080")
 
@@ -41,9 +46,88 @@ func main() {
 func getTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	w.Header().Set("Content-Type", "application/json")
-
 	encoder := json.NewEncoder(w)
 	encoder.SetIndent("", "    ")
 	encoder.Encode(tasks)
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+
+	var task Task
+
+	err := json.NewDecoder(r.Body).Decode(&task)
+	if err != nil {
+		http.Error(w, "JSON inválido", http.StatusBadRequest)
+		return
+	}
+
+	task.ID = nextID
+	nextID++
+
+	tasks = append(tasks, task)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	encoder := json.NewEncoder(w)
+	encoder.SetIndent("", "    ")
+	encoder.Encode(task)
+}
+func updateTask(w http.ResponseWriter, r *http.Request) {
+
+	idParam := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest)
+		return
+	}
+
+	var updatedTask Task
+
+	err = json.NewDecoder(r.Body).Decode(&updatedTask)
+	if err != nil {
+		http.Error(w, "JSON inválido", http.StatusBadRequest)
+		return
+	}
+
+	for i, task := range tasks {
+
+		if task.ID == id {
+
+			tasks[i].Title = updatedTask.Title
+			tasks[i].Done = updatedTask.Done
+
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(tasks[i])
+			return
+		}
+	}
+
+	http.Error(w, "Tarefa não encontrada", http.StatusNotFound)
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+	idParam := chi.URLParam(r, "id")
+
+	id, err := strconv.Atoi(idParam)
+	if err != nil {
+		http.Error(w, "ID inválido", http.StatusBadRequest)
+		return
+	}
+
+	for i, task := range tasks {
+
+		if task.ID == id {
+
+			tasks = append(tasks[:i], tasks[i+1:]...)
+
+			w.WriteHeader(http.StatusNoContent)
+
+			return
+		}
+	}
+
+	http.Error(w, "Tarefa não encontrada", http.StatusNotFound)
 }
